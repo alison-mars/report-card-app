25d6f126486c2d6443b2ba11ba8091a0ce6b8336
Nithish Raja <nithishraja248@gmail.com>
2026-05-12 15:23:02 +0530

    u

--- backend/index.ts
+++ backend/index.ts
@@ -38,6 +38,8 @@ app.use("/api/classrooms", classroomRouter);
 app.use("/api/tests", testRouter);
 app.use("/api/file", fileRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
